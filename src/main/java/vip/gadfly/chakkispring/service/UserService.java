66c410af6b7f6c78a9b42f0c9238705608902ed7
Gadfly <gadfly@gadfly.vip>
2020-12-06 23:06:26 +0800

    防止删除root用户

--- src/main/java/vip/gadfly/chakkispring/service/UserService.java
+++ src/main/java/vip/gadfly/chakkispring/service/UserService.java
@@ -170,4 +170,11 @@ public interface UserService extends IService<UserDO> {
      * @return 数据页
      */
     IPage<UserDO> getFreshTeacherPageByClassIdAndName(Page pager, Long classId, String name);
+
+    /**
+     * 获取超级管理员的id
+     *
+     * @return 超级管理员的id
+     */
+    Long getRootUserId();
 }
