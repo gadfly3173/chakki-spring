22955c172cda99997468f3c57b1d0459c9f81071
Gadfly <gadfly@gadfly.vip>
2021-01-29 16:15:39 +0800

    cleanup

--- src/main/java/vip/gadfly/chakkispring/dto/admin/AddStudentClassDTO.java
+++ src/main/java/vip/gadfly/chakkispring/dto/admin/AddStudentClassDTO.java
@@ -2,7 +2,10 @@ package vip.gadfly.chakkispring.dto.admin;
 
 import lombok.Data;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 /**
